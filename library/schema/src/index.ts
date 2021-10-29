404512a978b54a3e2a47a817cd6e7ec79ad73454
Karel Klíma <karelklima@gmail.com>
2021-10-29 13:20:46 +0200

    Fixed type exports from schema package

--- library/schema/src/index.ts
+++ library/schema/src/index.ts
@@ -4,12 +4,11 @@ export type {
   SchemaInterfaceType,
 } from "./interface";
 
-import type {
+export type {
   Schema,
   SchemaPrototype,
   Property,
   PropertyPrototype,
 } from "./schema";
-export { Schema, SchemaPrototype, Property, PropertyPrototype };
 
 export { expandSchema, getSchemaProperties } from "./utils";
