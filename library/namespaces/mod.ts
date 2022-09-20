3a2fbfebc5793bc1b18c76e85b5ed512593b2709
Karel Klíma <karelklima@gmail.com>
2022-09-20 16:53:51 +0200

    Added DBpedia ontology

--- library/namespaces/mod.ts
+++ library/namespaces/mod.ts
@@ -1,10 +1,11 @@
-export { default as rdf } from "./rdf.ts";
-export { default as rdfs } from "./rdfs.ts";
+export { default as dbo } from "./dbo.ts";
 export { default as dc } from "./dc.ts";
 export { default as dcterms } from "./dcterms.ts";
 export { default as foaf } from "./foaf.ts";
 export { default as gr } from "./gr.ts";
 export { default as ldkit } from "./ldkit.ts";
+export { default as rdf } from "./rdf.ts";
+export { default as rdfs } from "./rdfs.ts";
 export { default as schema } from "./schema.ts";
 export { default as sioc } from "./sioc.ts";
 export { default as skos } from "./skos.ts";
