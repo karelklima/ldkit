4d19535221712721a9df3e820aff30b1ee92ae02
Karel Klíma <karelklima@gmail.com>
2022-09-20 15:09:34 +0200

    Added GoodRelations namespace

--- library/namespaces/mod.ts
+++ library/namespaces/mod.ts
@@ -2,6 +2,8 @@ export { default as rdf } from "./rdf.ts";
 export { default as rdfs } from "./rdfs.ts";
 export { default as dc } from "./dc.ts";
 export { default as dcterms } from "./dcterms.ts";
+export { default as foaf } from "./foaf.ts";
+export { default as gr } from "./gr.ts";
 export { default as ldkit } from "./ldkit.ts";
 export { default as schema } from "./schema.ts";
 export { default as skos } from "./skos.ts";
