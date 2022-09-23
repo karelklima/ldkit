fc1ce07cc7d28f0444aafbbb3b89203239f4e65c
Karel Klíma <karelklima@gmail.com>
2022-09-23 18:05:44 +0200

    Added 404 documentation page

--- www/fresh.gen.ts
+++ www/fresh.gen.ts
@@ -4,16 +4,18 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/docs/[...slug].tsx";
-import * as $2 from "./routes/gfm.css.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/docs/[...slug].tsx";
+import * as $3 from "./routes/gfm.css.ts";
+import * as $4 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
-    "./routes/docs/[...slug].tsx": $1,
-    "./routes/gfm.css.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/_404.tsx": $1,
+    "./routes/docs/[...slug].tsx": $2,
+    "./routes/gfm.css.ts": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ www/routes/_404.tsx
@@ -0,0 +1,17 @@
+import { UnknownPageProps } from "$fresh/server.ts";
+
+import { App } from "../components/App.tsx";
+import { Jumbo } from "../components/Jumbo.tsx";
+
+export default function NotFoundPage({ url }: UnknownPageProps) {
+  return (
+    <App activeLink="/">
+      <Jumbo>Oops!</Jumbo>
+      <p class="text-center text-xl leading-10 mt-4">
+        Something went wrong, the page you requested is missing!
+        <br />
+        Hopefully we will find it soon.
+      </p>
+    </App>
+  );
+}
