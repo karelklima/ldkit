ba7b3848c8ae8cb1870e4f4a0638143a0731016d
karelklima <karelklima@gmail.com>
2023-03-10 15:14:33 +0100

    Updated Fresh to newest version

--- www/utils/markdown.ts
+++ www/utils/markdown.ts
@@ -3,5 +3,3 @@ import "https://esm.sh/prismjs@1.27.0/components/prism-jsx.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-typescript.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-tsx.js?no-check";
 import "https://esm.sh/prismjs@1.27.0/components/prism-diff.js?no-check";
-
-export { extract as frontMatter } from "$std/encoding/front_matter.ts";
