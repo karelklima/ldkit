6744c38d8c6dc945c37a1e3896e22288d5b42077
Karel Klíma <karelklima@gmail.com>
2024-01-04 21:56:02 +0100

    Refactored tests

--- tests/e2e/dbpedia.test.ts
+++ tests/e2e/dbpedia.test.ts
@@ -1,26 +1,14 @@
-import { assert } from "./test_deps.ts";
+import { assert } from "../test_deps.ts";
+import { logQuery } from "../test_utils.ts";
 
-import {
-  createLens,
-  createNamespace,
-  type Options,
-  type SchemaInterface,
-} from "ldkit";
+import { createLens, createNamespace } from "ldkit";
 import { rdfs, xsd } from "ldkit/namespaces";
 
-export const dbo = createNamespace(
+const dbo = createNamespace(
   {
     iri: "http://dbpedia.org/ontology/",
     prefix: "dbo:",
     terms: ["Person", "abstract", "thumbnail", "birthDate", "birthName"],
-  } as const,
-);
-
-export const dbp = createNamespace(
-  {
-    iri: "http://dbpedia.org/property/",
-    prefix: "dbp:",
-    terms: ["name"],
   } as const,
 );
 
@@ -36,25 +24,20 @@ const ActorSchema = {
   birthName: dbo.birthName,
 } as const;
 
-export const createLanguageContext = (language: string) => ({
-  sources: ["https://dbpedia.org/sparql"],
-  language,
-} as Options);
+const Actors = createLens(ActorSchema, {
+  source: "https://dbpedia.org/sparql",
+  language: "en",
+  logQuery,
+});
 
-export type ActorInterface = SchemaInterface<typeof ActorSchema>;
-
-export const Actors = createLens(ActorSchema, createLanguageContext("en"));
-
-export const favouriteActors = [
+const favouriteActors = [
   "http://dbpedia.org/resource/Brad_Pitt",
   "http://dbpedia.org/resource/Karl_Urban",
   "http://dbpedia.org/resource/Margot_Robbie",
   "http://dbpedia.org/resource/Gal_Gadot",
 ];
 
-export const languages = ["en", "cs", "ru", "de"];
-
-Deno.test("E2E / Query single remote entity", async () => {
+Deno.test("E2E / DBpedia / Query single remote entity", async () => {
   const BradId = "http://dbpedia.org/resource/Brad_Pitt";
   const BradName = "Brad Pitt";
   const actor = await Actors.findByIri(favouriteActors[0]);
@@ -63,7 +46,18 @@ Deno.test("E2E / Query single remote entity", async () => {
   assert(actor.name === BradName);
 });
 
-Deno.test("E2E / Query count", async () => {
+Deno.test("E2E / DBpedia / Query multiple specific remote entities", async () => {
+  const actors = await Actors.findByIris(favouriteActors);
+  assert(actors);
+  assert(actors.length === favouriteActors.length);
+});
+
+Deno.test("E2E / DBpedia / Query multiple random remote entities", async () => {
+  const actors = await Actors.find({ take: 7 });
+  assert(actors.length == 7);
+});
+
+Deno.test("E2E / DBpedia / Query count", async () => {
   const count = await Actors.count();
   assert(count > 0);
 });
