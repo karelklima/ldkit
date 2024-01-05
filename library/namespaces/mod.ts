35588204d84d00f94a9d12d4addb836839dafdf2
Karel Klíma <karelklima@gmail.com>
2024-01-05 15:42:48 +0100

    Added OWL namespace

--- library/namespaces/mod.ts
+++ library/namespaces/mod.ts
@@ -4,6 +4,7 @@ export { default as dcterms } from "./dcterms.ts";
 export { default as foaf } from "./foaf.ts";
 export { default as gr } from "./gr.ts";
 export { default as ldkit } from "./ldkit.ts";
+export { default as owl } from "./owl.ts";
 export { default as rdf } from "./rdf.ts";
 export { default as rdfs } from "./rdfs.ts";
 export { default as schema } from "./schema.ts";
