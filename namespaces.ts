8b566bec875fe71750cd9cba72ed2630442ed270
Karel Klíma <karelklima@gmail.com>
2024-01-07 17:43:45 +0100

    Improved namespaces module documentation

--- namespaces.ts
+++ namespaces.ts
@@ -1,3 +1,19 @@
+/**
+ * Popular namespaces used in Linked Data, fully compatible with LDkit,
+ * offering autocompletion and type checking in IDE.
+ *
+ * Create your own namespaces using {@link createNamespace} helper.
+ *
+ * @example
+ * ```typescript
+ * import { rdf, schema } from "ldkit/namespaces";
+ *
+ * console.log(rdf.type); // "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+ * console.log(schema.Person); // "http://schema.org/Person"
+ * ```
+ *
+ * @module
+ */
 export { dbo } from "./namespaces/dbo.ts";
 export { dc } from "./namespaces/dc.ts";
 export { dcterms } from "./namespaces/dcterms.ts";
