b2c338c90d210a010fe610c5ece0f85757529680
Karel Klíma <karelklima@gmail.com>
2024-01-07 22:30:45 +0100

    Improved QueryEngine documentation

--- library/engine/types.ts
+++ library/engine/types.ts
@@ -5,11 +5,30 @@ import type {
 
 import { RDF } from "../rdf.ts";
 
+/**
+ * A set of context entries that can be passed to a query engine,
+ * such as data sources, fetch configuration, etc.
+ *
+ * @example
+ * ```typescript
+ * import { QueryContext, QueryEngine } from "ldkit";
+ *
+ * const context: QueryContext = {
+ *   sources: ["https://dbpedia.org/sparql"],
+ * };
+ *
+ * const engine = new QueryEngine();
+ * await engine.queryBoolean("ASK { ?s ?p ?o }", context);
+ * ```
+ */
 export type QueryContext =
   & RDF.QueryStringContext
   & RDF.QuerySourceContext<IDataSource>
   & IQueryContextCommon;
 
+/**
+ * Interface of a query engine compatible with LDkit
+ */
 export type IQueryEngine = RDF.StringSparqlQueryable<
   RDF.SparqlResultSupport,
   QueryContext
