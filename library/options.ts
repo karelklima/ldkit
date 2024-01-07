8e1a68eb536bc715e5058e3e3d26e4a4c6c9651c
Karel Klíma <karelklima@gmail.com>
2024-01-07 22:42:21 +0100

    Improved Options documentation

--- library/options.ts
+++ library/options.ts
@@ -4,6 +4,15 @@ import {
   QueryEngine,
 } from "./engine/mod.ts";
 
+/**
+ * LDkit options and query engine context
+ *
+ * LDkit-specific options are:
+ * - `engine` - a query engine to use for querying data sources
+ * - `language` - a preferred language for literals
+ * - `take` - a default number of results to take (limit of SELECT queries)
+ * - `logQuery` - a function that will be called for each SPARQL query
+ */
 export type Options = {
   engine?: IQueryEngine;
   language?: string;
@@ -19,6 +28,28 @@ const defaultOptions = {
 
 let globalOptions: Options = {};
 
+/**
+ * Sets global configuration {@link Options} for LDkit that will be used
+ * by default in all queries, unless overridden in {@link Lens}.
+ *
+ * LDkit-specific options are:
+ * - `engine` - a query engine to use for querying data sources
+ * - `language` - a preferred language for literals
+ * - `take` - a default number of results to take (limit of SELECT queries)
+ * - `logQuery` - a function that will be called for each SPARQL query
+ *
+ * Default values for these options are:
+ * ```typescript
+ * const defaultOptions = {
+ *   engine: new QueryEngine(),
+ *   take: 1000,
+ *   logQuery: () => {},
+ * };
+ * ```
+ * The default configuration uses built-in {@link QueryEngine}. Language is not set by default.
+ *
+ * @param options LDkit options and query engine context
+ */
 export function setGlobalOptions(options: Options): void {
   globalOptions = options;
 }
