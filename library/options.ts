0e8894d59944ad66127e3fe093f7130ec6467e6e
Karel Klíma <karelklima@gmail.com>
2025-08-06 11:07:07 +0200

    Re-added filtering of duplicate quads (#156)

--- library/options.ts
+++ library/options.ts
@@ -24,7 +24,6 @@ const defaultOptions = {
   engine: new QueryEngine(),
   take: 1000,
   logQuery: () => {},
-  distinctConstruct: true, // Needed for Comunica to filter duplicate quads on CONSTRUCT queries
 };
 
 let globalOptions: Options = {};
